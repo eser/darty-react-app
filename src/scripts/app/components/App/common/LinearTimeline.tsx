21269f85a0531f44574ec36342d2ab03b083eb8c
Eser Ozvataf <eser@ozvataf.com>
2016-08-22 08:31:54 +0300

    * improved nested rendering.

--- src/scripts/app/components/App/common/LinearTimeline.tsx
+++ src/scripts/app/components/App/common/LinearTimeline.tsx
@@ -14,46 +14,52 @@ export class LinearTimeline extends React.Component<any, any> {
     public render() {
         const data = this.props.datasource[this.props.datakey];
 
-        let output = [];
-        for (const year in data) {
-            const yearKey = `year.${encodeURIComponent(year)}`;
-
-            output.push(<h3 key={`${yearKey}.caption`}><Link key={`${yearKey}.link`} to={`/categories/year/${encodeURIComponent(year)}`}>{year}</Link></h3>);
-            output.push(
-                <ul key={`${yearKey}.list`}>
-                    {Object.keys(data[year]).forEach((event) => {
-                        if (event === '_items') {
-                            return;
-                        }
-
-                        const eventKey = `year.${year}.event.${encodeURIComponent(event)}`;
-
-                        output.push(
-                            <li key={eventKey}>
-                                <h4 key={`${eventKey}.caption`}><Link key={`${eventKey}.caption.link`} to={`/categories/event/${encodeURIComponent(event)}`}>{event}</Link></h4>
-                                <ul key={`${eventKey}.list`}>
-                                    {data[year][event]._items.map((item) => {
-                                        const entryKey = `entry.${encodeURIComponent(item.entry)}`;
-
-                                        return (<li key={entryKey}><LinearTimelineItem key={`${entryKey}.item`} item={item} /></li>);
-                                    })}
-                                </ul>
-                            </li>
-                        );
-                    })}
-                    {data[year]._items.map((item) => {
-                        const entryKey = `entry.${encodeURIComponent(item.entry)}`;
-
-                        return (<li key={entryKey}><LinearTimelineItem key={`${entryKey}.item`} id={`${entryKey}.item`} item={item} /></li>);
-                    })}
-                </ul>
-            );
-        }
-
         return (
-            <div>
-                {output}
-            </div>
+            <ul>
+                {Object.keys(data).map((year) => {
+                    const yearKey = `year.${encodeURIComponent(year)}`,
+                        yearData = data[year];
+
+                    return (
+                        <li key={yearKey}>
+                            <h3 key={`${yearKey}.caption`}><Link key={`${yearKey}.link`} to={`/categories/year/${encodeURIComponent(year)}`}>{year}</Link></h3>
+
+                            <ul key={`${yearKey}.list`}>
+                                {Object.keys(yearData).map((event) => {
+                                    if (event === '_items') {
+                                        return null;
+                                    }
+
+                                    const eventKey = `year.${year}.event.${encodeURIComponent(event)}`,
+                                        eventData = yearData[event];
+
+                                    return (
+                                        <li key={eventKey}>
+                                            <h4 key={`${eventKey}.caption`}><Link key={`${eventKey}.caption.link`} to={`/categories/event/${encodeURIComponent(event)}`}>{event}</Link></h4>
+                                            <ul key={`${eventKey}.list`}>
+                                                {eventData._items.map((item) => {
+                                                    const entryKey = `entry.${encodeURIComponent(item.entry)}`;
+
+                                                    return (
+                                                        <li key={entryKey}><LinearTimelineItem key={`${entryKey}.item`} item={item} /></li>
+                                                    );
+                                                })}
+                                            </ul>
+                                        </li>
+                                    );
+                                })}
+                                {yearData._items.map((item) => {
+                                    const entryKey = `entry.${encodeURIComponent(item.entry)}`;
+
+                                    return (
+                                        <li key={entryKey}><LinearTimelineItem key={`${entryKey}.item`} id={`${entryKey}.item`} item={item} /></li>
+                                    );
+                                })}
+                            </ul>
+                        </li>
+                    );
+                })}
+            </ul>
         );
     }
 
--- src/scripts/app/components/App/common/PageList.tsx
+++ src/scripts/app/components/App/common/PageList.tsx
@@ -14,10 +14,25 @@ export class PageList extends React.Component<any, any> {
 
         return (
             <ul>
-                {data.map((page) => {
-                    const pageKey = `page.${encodeURIComponent(page.name)}`;
+                {Object.keys(data).map((type) => {
+                    const typeKey = `type.${encodeURIComponent(type)}`,
+                        typeData = data[type];
 
-                    return <li key={`${pageKey}`}><Link key={`${pageKey}.link`} to={`/pages/${encodeURIComponent(page.name)}`}>{page.name}</Link></li>;
+                    return (
+                        <li key={typeKey}>
+                            <h3 key={`${typeKey}.caption`}>{type}</h3>
+
+                            <ul key={`${typeKey}.list`}>
+                                {typeData.map((page) => {
+                                    const pageKey = `page.${encodeURIComponent(page.name)}`;
+
+                                    return (
+                                        <li key={`${typeKey}.page.${pageKey}`}><Link key={`${typeKey}.page.${pageKey}.link`} to={`/pages/${encodeURIComponent(page.name)}`}>{page.name}</Link></li>
+                                    );
+                                })}
+                            </ul>
+                        </li>
+                    );
                 })}
             </ul>
         );
