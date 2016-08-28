2dd13ebc6cbb13b2b43e317926d3d7308d6c911b
Eser Ozvataf <eser@ozvataf.com>
2016-08-28 03:36:11 +0300

    * markdown support.

--- src/scripts/app/components/App/common/LinearTimelineItem.tsx
+++ src/scripts/app/components/App/common/LinearTimelineItem.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Link } from 'react-router';
 import * as ReactMarkdown from 'react-markdown';
 
 export class LinearTimelineItem extends React.Component<any, any> {
